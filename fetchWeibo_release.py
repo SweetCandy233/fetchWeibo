f3a3dd79c66a8aa022cff3f7abea9e295adcdb90
SweetCandy233 <68184088+SweetCandy233@users.noreply.github.com>
2023-08-15 22:39:09 +0800

    v0.3.8 update

--- fetchWeibo_release.py
+++ fetchWeibo_release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import requests, time, os, gc, win32com.client, linecache, ctypes, subprocess, sys, json, logging, webbrowser
+import requests, time, os, gc, win32com.client, linecache, ctypes, subprocess, sys, json, logging, webbrowser, platform
 from win10toast_click import ToastNotifier
 
 toaster = ToastNotifier()
@@ -10,12 +10,13 @@ windirPath = os.environ['windir']
 tempPath = os.environ['temp']
 firstRun = True
 speaker = win32com.client.Dispatch('SAPI.SpVoice')
-updateUrl = 'https://maybeiamcandy.github.io/version/fetchWeibo/version.json'
-bakUpdateUrl = 'https://www.itsnotch404.top/fetchWeibo/version.json'
+updateUrl = 'http://lh2.hkg-ali.server.sweetcandy233.top/assets/fetchWeibo/version.json'
+bakUpdateUrl = 'http://api-production.na-ms.server.sweetcandy233.top/assets/fetchWeibo/version.json'
 url = 'https://weibo.com/ceic'
 # url = 'http://127.0.0.1:8000/weibo_ceic.html'
 # headers = {'User-Agent': 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
 headers = {'User-Agent': 'Mozilla/5.0(compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)'}
+appHeaders = {'User-Agent': 'Mozilla/5.0 (compatible; fetchWeiboApp/0.3.8)'}
 # baseName = os.path.basename(__file__).split('.')[0]+'.exe'
 baseName = 'fetchWeibo'
 LOG_FORMAT = "[%(asctime)s/%(levelname)s] %(message)s"
@@ -24,11 +25,18 @@ logging.basicConfig(filename='latest.log', encoding='utf-8', level=logging.INFO,
 
 appInfo = """{
     "name": "fetchWeibo",
-    "version": "0.3.7",
-    "time": 1677247617
+    "version": "0.3.8",
+    "time": 1688376308
 }"""
 
 appInfo = json.loads(appInfo)
+
+def platform_check():
+# > 10.0.10240 counts as Windows 10
+# > 10.0.22000 counts as Windows 11
+    if ('Windows-10' or 'Windows-11') not in platform.platform():
+        Mbox('注意', '您的操作系统版本可能不受支持，目前仅支持Windows 10及以上版本，若无法正常使用程序，请确定使用的系统符合条件。若仍有问题，请截图反馈至开发者。\n'+platform.platform()+'\n'+platform.version()+'\nfetchWeiboApp/0.3.8', 48)
+platform_check()
 
 def isAdmin():
     try:
@@ -55,7 +63,7 @@ time.sleep(1)
 console = ctypes.windll.kernel32.GetConsoleWindow()
 if console != 0:
     ctypes.windll.user32.ShowWindow(console, 0)
-    ctypes.windll.kernel32.CloseHandle(console) #隐藏窗口
+    ctypes.windll.kernel32.CloseHandle(console) # 隐藏窗口
 
 def init_volume():
     # recommended value: 70
@@ -67,7 +75,7 @@ def init_volume():
         f = open(vFileName, 'r', encoding='utf-8')
         logging.info('TTS语音合成 - 配置文件存在，读取配置')
     linecache.updatecache(vFileName)
-    set_volume = linecache.getline(vFileName, 1) #到底为什么日志会多出一行空行呢
+    set_volume = linecache.getline(vFileName, 1) # 到底为什么日志会多出一行空行呢
     try:
         int(set_volume)
         if (int(set_volume) >= 0) and (int(set_volume) <= 100):
@@ -98,8 +106,10 @@ def push_notification():
     first = lines[0]
 
     print('first:', first)
-    data = lines[0].split('：', 1)
-
+    if '：' in lines[0]: # 正常格式，使用冒号进行截取
+        data = lines[0].split('：', 1)
+    else: # 人工发布格式，对第一个逗号进行截取，取逗号后面的内容作为正文
+        data = lines[0].split('，', 1)
     autoFlag = False
     depthAutoFlag = False
     if '自动' in lines[0]:
@@ -121,13 +131,26 @@ def push_notification():
 
     print('data:', data)
     line = data[0]
-    message = data[1].rstrip('\n')
+    try:
+        message = data[1].rstrip('\n')
+        if '若有震感' in message:
+            message = message.split('若有震感')[0]
+    except IndexError: # 格式异常时使用a标签作为结束判断
+        message = data[0].strip('（<a')[0]
+        if '若有震感' in message:
+            message = message.split('若有震感')[0]
     if depthAutoFlag:
         message_split = message.split('，')
         msg1 = message_split[0]
         msg2 = message_split[1]
         # msgList 已修改，待检验，message_split[2]始终是“最终结果以正式速报为准。”
-        msgList = [msg1, '，', msg2, '，', '震源深度{}千米'.format(depthAuto), '，', message_split[2]]
+        try:
+            depthAuto
+        except UnboundLocalError:
+            print('自动报信息不匹配，忽略自动报深度信息。')
+            msgList = [msg1, '，', msg2, '，', message_split[2]]
+        else:
+            msgList = [msg1, '，', msg2, '，', '震源深度{}千米'.format(depthAuto), '，', message_split[2]]
         final_content = ''.join(msgList)
         print(final_content)
     else:
@@ -164,12 +187,12 @@ def new_file(fn, method, encoding, content):
 
 def update_check():
     try:
-        update = requests.get(url=updateUrl, headers=headers)
+        update = requests.get(url=updateUrl, headers=appHeaders)
     except Exception as e:
         print('连接失败：{}'.format(str(e)))
         logging.warning('无法连接到更新服务器')
         try:
-            update = requests.get(url=bakUpdateUrl, headers=headers)
+            update = requests.get(url=bakUpdateUrl, headers=appHeaders)
         except Exception as e:
             print('连接失败：{}'.format(str(e)))
             logging.warning('无法连接到备用更新服务器')
@@ -177,10 +200,15 @@ def update_check():
             pass
     else:
         result = json.loads(update.text)
+
+        if result['status']['toggle'] == 'off':
+            Mbox('公告', '{}'.format(result['status']['em_announcement']), 48)
+            sys.exit(1)
+
         print(result['time'])
         if appInfo['time'] < (result['time']):
             print('有新版本可用！')
-            Mbox('{} 更新检测', '目前版本{}，最新版本{}\n更新内容：\n{}\n点击确定以继续运行程序'.format(baseName, appInfo['version'], result['version'], result['desc']), 64)
+            Mbox('更新检测', '目前版本{}，最新版本{}\n更新内容：\n{}\n点击确定以继续运行程序'.format(appInfo['version'], result['version'], result['desc']), 64)
             logging.info('更新检测 - 检测到新版本：{}，目前版本：{}'.format(result['version'], appInfo['version']))
         else:
             print('目前已是最新版本！')
@@ -242,9 +270,15 @@ while True:
     fileIsWritten = True
     for line in f.readlines():
         if '#地震快讯#' in line:
-            if '中国地震台网' in line:
+            if '据' in line:
+                line = line.split(' 据')[1] #开始判断
+                line = line.split('（<a ')[0] #结束判断
+            elif '中国地震台网' in line:
                 line = line.split('（ <a', 1)[0] #结束判断
                 line = line.split('#地震快讯#</a>', 1)[1] #开始判断
+                #if '若有震感' in line:
+                #    line.split('若有震感')[0] #结束判断
+                
             else:
                 logging.info('检测到指定tag，但tag信息不符合，等待10秒后重试')
                 print('内容不匹配 等待重试')
